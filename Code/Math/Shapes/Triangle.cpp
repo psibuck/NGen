a5439f97ed2eb052d4fd7b4f58ad02f65369d8d1
Archie <psibuck@gmail.com>
2019-04-14 09:19:25 +0100

    Not sure entirely, I made some adjustments cba to diff, everything seems to work but no changes for a few weeks

---
+++ Code/Math/Shapes/Triangle.cpp
@@ -0,0 +1,9 @@
+//
+//  Triangle.cpp
+//  Emily
+//
+//  Created by Archie Buck on 19/03/2019.
+//  Copyright © 2019 Archie Buck. All rights reserved.
+//
+
+#include "Triangle.hpp"
---
+++ Code/Math/Shapes/Triangle.h
@@ -0,0 +1,14 @@
+//
+//  Triangle.h
+//  Emily
+//
+//  Created by Archie Buck on 19/03/2019.
+//  Copyright © 2019 Archie Buck. All rights reserved.
+//
+
+#ifndef Triangle_hpp
+#define Triangle_hpp
+
+#include <stdio.h>
+
+#endif /* Triangle_hpp */
